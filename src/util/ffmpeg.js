a23ff02c6fd6d19c9362d8b51fb1fc6c193be498
Nathan Vegdahl <cessen@cessen.com>
2019-08-14 11:45:28 +0900

    Change exported images to be smaller and lower quality.
    They're just visual reminders for context, so they don't need to
    be high quality.  And this saves on space and sync bandwidth.

--- src/util/ffmpeg.js
+++ src/util/ffmpeg.js
@@ -47,7 +47,7 @@ export const extractFrameImage = async (vidfn, time) => {
   const tmpfile = await tmp.file({keep: true, postfix: '.jpg'});
 
   await new Promise((resolve, reject) => {
-    const subp = spawn(getBinaryFilename(), ['-ss', time.toString(), '-i', vidfn, '-vf', "scale='min(854,iw)':'min(480,ih)':force_original_aspect_ratio=decrease", '-frames:v', '1', '-y', tmpfile.path], {windowsHide: true, stdio: ['ignore', 'pipe', 'pipe']});
+    const subp = spawn(getBinaryFilename(), ['-ss', time.toString(), '-i', vidfn, '-vf', "scale='min(480,iw)':'min(240,ih)':force_original_aspect_ratio=decrease", '-frames:v', '1', "-q:v", "6", '-y', tmpfile.path], {windowsHide: true, stdio: ['ignore', 'pipe', 'pipe']});
 
     subp.on('error', (error) => {
       reject(error);
