a3ce901f3f0d7550c43a38ed4f692d782fbb473c
tscheney <thomascheneyyzs@gmail.com>
2024-03-12 22:51:16 -0700

    Update packages

--- src/ruxx/connect.js
+++ src/ruxx/connect.js
@@ -4,8 +4,6 @@ export default class StateMapper extends Component {
   constructor(props) {
     super(props);
 
-    // NOTE: At the time of writing, according to this thread (https://github.com/facebook/react/issues/7671) FB is planning to discourage the use of componentWillMount, and the recommended technique is to do initialization in the constructor.
-
     // Initialize component state to be "actual" state
     this.state = {actualState: this.props.subscribableState.get()};
 
