d1c5f4d780a6a99504e22ac99150683b3857055e
tscheney <thomascheneyyzs@gmail.com>
2024-03-15 00:17:22 -0700

    Remove deprecated callback to get rid of warning

--- app/electron-main.js
+++ app/electron-main.js
@@ -30,9 +30,7 @@ function registerLocalProtocol() {
     const path = request.url.substr(8); // strip off local:// prefix
     const decodedPath = decodeURI(path); // this makes utf-8 filenames work, I'm not sure it's correct
     callback(decodedPath);
-  }, (error) => {
-    if (error) console.error('Failed to register protocol');
-  })
+  });
 }
 
 function resolveHtmlPath(htmlFileName) {
