177abb03f1a65494ea2b545c1eb4734daad01fc7
tscheney <thomascheneyyzs@gmail.com>
2024-05-18 21:42:25 -0700

    Use whitelisting for ipc for security

--- src/main/preload.js
+++ src/main/preload.js
@@ -6,27 +6,38 @@ const {
 contextBridge.exposeInMainWorld('api', {
             send: (channel, data) => {
                 // whitelist channels
-                //let validChannels = ["toMain"];
-                //if (validChannels.includes(channel)) {
-                    ipcRenderer.send(channel, data);
-                //}
+              let validChannels = [
+                'openDevTools', 'toggleFullscreen', 'windowExit', 'tmpFileCleanup',
+                'choose-video-file'
+              ];
+              if (validChannels.includes(channel)) {
+                  ipcRenderer.send(channel, data);
+              }
             },
             receive: (channel, func) => {
-                //if (Object.values(CONTENT_EVENTS.E2C).includes(channel)) {
-                  // Deliberately strip event as it includes `sender` 
-                  const subscription = (event, ...args) => func(...args);
-                  ipcRenderer.on(channel, subscription);
-                  return () => {
-                    ipcRenderer.removeListener(channel, subscription);
-                  }
-                //}
+              let validChannels = [];
+              if (validChannels.includes(channel)) {
+                // Deliberately strip event as it includes `sender` 
+                const subscription = (event, ...args) => func(...args);
+                ipcRenderer.on(channel, subscription);
+                return () => {
+                  ipcRenderer.removeListener(channel, subscription);
+                }
+              }
             },
             // From render to main and back again.
             invoke: (channel, ...args) => {
-                //let validChannels = ipc.render.sendReceive;
-                //if (validChannels.includes(channel)) {
-                    return ipcRenderer.invoke(channel, ...args);
-                //}
+              let validChannels = [
+                'appGetAppPath', 'appGetPath', 'appGetVersion', 'processPlatform',
+                'fsReadDirSync', 'fsStatIsDirectory', 'fsExistsSync', 'fsReadFileSync',
+                'fsUnlinkSync', 'fsEnsureDirSync', 'processArgvIncludes', 'pathBasename',
+                'pathJoin', 'pathExtname', 'pathParse', 'assert', 'shellOpenExternal',
+                'childProcessExecFile', 'createMedia', 'tmpFile', 'getSubData', 'dialog',
+                'dbInitialize', 'dbRun', 'dbGet', 'dbAll'
+              ];
+              if (validChannels.includes(channel)) {
+                  return ipcRenderer.invoke(channel, ...args);
+              }
             }
         }
     );
